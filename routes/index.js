fa2db5a6e60246bd5d95d0647de8e6c9829caab7
Denis Travis <54004935+denistravis@users.noreply.github.com>
2020-10-05 18:19:37 +0300

    remove

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,229 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const transport = require('../../plugins/mailtransporter');
+const checkOrigin = require("../../plugins/checkOrigin");
+const deletePendingUserTemplate = require('../../templates/delete/pending/toAll');
+const jwtVerify = require('../../plugins/jwtVerify');
 
 //Model Imports
-const User = require("../models/user");
-const PendingUser = require("../models/pendingUser");
-const SpamUser = require("../models/spamUser");
-const InvitedUser = require("../models/invitedUser");
-const checkOrigin = require("../plugins/checkOrigin");
+const User = require("../../models/user");
+const PendingUser = require("../../models/pendingUser");
 
-router.get('/', function(req, res){
-	User.findOne({ superadmin: true }, function(error, result){
-		if(result){
-			res.render("dashboard.ejs", { user: false, showPass: false, data: "This is a Backend for G-Index. This has Been Already Setup. So Nothing Exists Here Afterwards. Use Your Frontend to Communicate.", fronturl: process.env.FRONTENDURL.split(",")[0] });
-		} else {
-			res.render("signup.ejs");
-		}
-	})
-});
-
-router.get('/generate', function(req, res){
-	res.render("generate.ejs");
-});
-
-router.post('/generate', function(req, res){
-	bcrypt.hash(req.body.password, 10, function(err, hashedPass){
-		if(hashedPass){
-			res.render("dashboard.ejs", {user: false, showPass: true, hybrid: hashedPass})
-		}
-	})
-});
-
-router.post('/checkmail', function(req, res){
+router.post('/user', function(req, res){
 	if(checkOrigin(req.headers.origin)){
-		SpamUser.findOne({ email: req.body.email }, function(err, result){
-			if(result){
-				res.status(200).send({ auth: false, user: false, status: "Spammed User" })
-			} else {
-				PendingUser.findOne({ email: req.body.email }, function(err ,result){
-					if(result){
-						res.status(200).send({ auth: false, user: false, status: "Pending Confirmation from Admins." })
-					} else {
-						User.findOne({ email: req.body.email }, function(err, result){
+		if(jwtVerify(req.headers.token)){
+			PendingUser.findOne({ email: req.body.email, post: "User" }, function(error, pendingResult){
+					if(pendingResult){
+						User.findOne({ email: req.body.adminuseremail }, function(error, result){
 							if(result){
-								if(result.verified){
-									res.status(200).send({ auth: true, user: true, status: "User Present & Verified" })
+								if(result.admin){
+									PendingUser.deleteOne({ email: req.body.email, post: "User" }, function(error){
+										if(error){
+											res.status(200).send({
+												auth: true,
+												removed: false,
+												message: "Some Error Processing Your Request. Please Try again Now."
+											});
+										} else {
+											const deleteMessage = {
+												 from: `"${process.env.FRONTENDSITENAME} - Support"<${process.env.EMAILID}>`,
+												 to: pendingResult.email,
+												 replyTo: process.env.REPLYTOMAIL,
+												 subject: 'Regarding Your Request',
+												 html: deletePendingUserTemplate(pendingResult),
+											};
+											transport.sendMail(deleteMessage, function(err, info){
+												if(err){
+													console.log(err);
+												} else {
+													console.log(info)
+												}
+											})
+											res.status(200).send({
+												auth: true,
+												removed: true,
+												message: "Request has been Deleted"
+											});
+										}
+									})
 								} else {
-									res.status(200).send({ auth: false, user:true, status: "User Present & Not Verified" })
+									res.status(200).send({
+										auth: true,
+										removed: false,
+										message: "You Need to be a Admin."
+									});
 								}
 							} else {
-								res.status(200).send({ auth: false, user: false, status: "User Not Present" })
+								res.status(200).send({
+									auth: true,
+									removed: false,
+									message: "You are Unauthorized."
+								});
 							}
 						})
+					} else {
+						res.status(200).send({
+							auth: true,
+							removed: false,
+							message: "BAD REQUEST"
+						});
 					}
 				})
-			}
-		})
+		} else {
+			res.status(200).send({ auth: false, message: "Bearer Token Not Valid" })
+		}
+	} else {
+		res.status(200).send({ auth: false, message: "UNAUTHORIZED" })
+	}
+})
+
+router.post('/admin', function(req, res){
+	if(checkOrigin(req.headers.origin)){
+		if(jwtVerify(req.headers.token)){
+			PendingUser.findOne({ email: req.body.email, post: "Admin" }, function(error, pendingResult){
+					if(pendingResult){
+						User.findOne({ email: req.body.adminemail }, function(error, result){
+							if(result){
+								if(result.admin){
+									if(result.superadmin){
+										PendingUser.deleteOne({ email: req.body.email, post: "Admin" }, function(error){
+											if(error){
+												res.status(200).send({
+													auth: true,
+													removed: false,
+													message: "Some Error Processing Your Request. Please Try again Now."
+												});
+											} else {
+												const deleteMessage = {
+													 from: `"${process.env.FRONTENDSITENAME} - Support"<${process.env.EMAILID}>`,
+													 to: pendingResult.email,
+													 replyTo: process.env.REPLYTOMAIL,
+													 subject: 'Regarding Your Request',
+													 html: deletePendingUserTemplate(pendingResult),
+												};
+												transport.sendMail(deleteMessage, function(err, info){
+													if(err){
+														console.log(err);
+													} else {
+														console.log(info)
+													}
+												})
+												res.status(200).send({
+													auth: true,
+													removed: true,
+													message: "Request has been Deleted"
+												});
+											}
+										})
+									} else {
+										res.status(200).send({
+											auth: true,
+											removed: false,
+											message: "You Need to be a SuperAdmin."
+										});
+									}
+								} else {
+									res.status(200).send({
+										auth: true,
+										removed: false,
+										message: "You Need to be a Admin."
+									});
+								}
+							} else {
+								res.status(200).send({
+									auth: true,
+									removed: false,
+									message: "You are Unauthorized."
+								});
+							}
+						})
+					} else {
+						res.status(200).send({
+							auth: true,
+							removed: false,
+							message: "BAD REQUEST"
+						});
+					}
+				})
+		} else {
+			res.status(200).send({ auth: false, message: "Bearer Token Not Valid" })
+		}
+	} else {
+		res.status(200).send({ auth: false, message: "UNAUTHORIZED" })
+	}
+})
+
+router.post('/superadmin', function(req, res){
+	if(checkOrigin(req.headers.origin)){
+		if(jwtVerify(req.headers.token)){
+			PendingUser.findOne({ email: req.body.email, post: "SuperAdmin" }, function(error, pendingResult){
+					if(pendingResult){
+						User.findOne({ email: req.body.adminemail }, function(error, result){
+							if(result){
+								if(result.admin){
+									if(result.superadmin){
+										PendingUser.deleteOne({ email: req.body.email, post: "SuperAdmin" }, function(error){
+											if(error){
+												res.status(200).send({
+													auth: true,
+													removed: false,
+													message: "Some Error Processing Your Request. Please Try again Now."
+												});
+											} else {
+												const deleteMessage = {
+													 from: `"${process.env.FRONTENDSITENAME} - Support"<${process.env.EMAILID}>`,
+													 to: pendingResult.email,
+													 replyTo: process.env.REPLYTOMAIL,
+													 subject: 'Regarding Your Request',
+													 html: deletePendingUserTemplate(pendingResult),
+												};
+												transport.sendMail(deleteMessage, function(err, info){
+													if(err){
+														console.log(err);
+													} else {
+														console.log(info)
+													}
+												})
+												res.status(200).send({
+													auth: true,
+													removed: true,
+													message: "Request has been Deleted"
+												});
+											}
+										})
+									} else {
+										res.status(200).send({
+											auth: true,
+											removed: false,
+											message: "You Need to be a SuperAdmin."
+										});
+									}
+								} else {
+									res.status(200).send({
+										auth: true,
+										removed: false,
+										message: "You Need to be a Admin."
+									});
+								}
+							} else {
+								res.status(200).send({
+									auth: true,
+									removed: false,
+									message: "You are Unauthorized."
+								});
+							}
+						})
+					} else {
+						res.status(200).send({
+							auth: true,
+							removed: false,
+							message: "BAD REQUEST"
+						});
+					}
+				})
+		} else {
+			res.status(200).send({ auth: false, message: "Bearer Token Not Valid" })
+		}
 	} else {
 		res.status(200).send({ auth: false, message: "UNAUTHORIZED" })
 	}
